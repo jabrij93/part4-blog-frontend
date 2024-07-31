bcb6aaa8c8997684ac0514bf2393ed1b43f9f732
jabss <jabrijuhinin93@gmail.com>
2024-08-01 01:11:40 +0800

    button hide additional info working

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,17 +1,68 @@
-const Blog = ({ blog, toggleImportance, index }) => {
-    // const label = blog.important ? 'make not important' : 'make important'
+import React from 'react';
+import { useState } from 'react'
 
-    return (
-      <div>
-         <p> {index} </p>
-        <p> Title : {blog.title} </p>
-        <p> Author : {blog.author} </p>
-        <p> Likes : {blog.likes} </p>
-        <p> Url : {blog.url} </p>
-        
-        {/* <button onClick={toggleImportance}> {label} </button> */}
-      </div>
-    )
+const Blog = ({ blog, toggleImportance, index }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
   }
-  
-  export default Blog
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const blogStyleWithBlock = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    display: 'block'
+  }
+
+  // const inlineBlock = {
+  //   display: 'block'
+  // }
+
+  // const label = blog.important ? 'make not important' : 'make important'
+  const buttonStyle = {
+    marginLeft: '10px',
+  };
+
+
+  return (
+    <div>
+      <div style={ blogStyle }>
+        <div style={{ display: 'inline-flex' }}>
+          <p style={{ marginRight: '10px', marginBottom: '0'}}> Title: {blog.title} </p> <button onClick={toggleVisibility}> view </button>
+        </div>
+        <div style={showWhenVisible}> 
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div>
+                <div style={{display: 'inline-flex'}}>
+                <p> Author: {blog.author} </p>
+                <button onClick={toggleVisibility} > hide </button>
+                </div>
+              
+                <p> Likes: {blog.likes} </p>
+          
+                <p> Url: {blog.url} </p>
+              </div>
+              
+            </div>
+        </div>
+      </div>
+      {/* <button onClick={toggleImportance}> {label} </button> */}
+    </div>
+  );
+};
+
+export default Blog;

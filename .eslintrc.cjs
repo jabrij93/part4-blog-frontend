09e1676c33cb1999617d5ee81e4e2a9f67dce79d
jabss <jabrijuhinin93@gmail.com>
2024-08-07 23:55:31 +0800

    added react and vitest plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,14 @@ module.exports = {
     env: {
       browser: true,
       es2021: true,
+      "vitest-globals/env": true,
     },
     extends: [
       'eslint:recommended',
       'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+      'plugin:vitest-globals/recommended',
     ],
     parserOptions: {
       ecmaFeatures: {

b05753b9454bfa3590a33d54d585b45180a1b769
jabss <jabrijuhinin93@gmail.com>
2024-08-07 23:58:17 +0800

    test react component using CSS-selectors usg querySelector method

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -6,9 +6,14 @@ test('renders content', () => {
     title: 'Component testing is done with react-testing-library'
   }
 
-  render(<Blog blog={blog} />)
+  const { container } = render(<Blog blog={blog} />)
+
+  const div = container.querySelector('.blog')
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
 
   // const element = screen.getByText((content, element) => element.tagName.toLowerCase() === 'p' && content.includes('Component testing is done with react-testing-library'))
-  const element = screen.getByText((content, element) => content.includes('Component testing is done with react-testing-library'))
-  expect(element).toBeDefined()
+  // const element = screen.getByText((content, element) => content.includes('Component testing is done with react-testing-library'))
+  // expect(element).toBeDefined()
 })
